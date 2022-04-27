6a7fddd049c4d142730fe7f9e806be0e32472f1b
Midstackdev <midstackdev@gmail.com>
2022-04-27 02:02:42 +0000

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,47 @@
+import { ChatBubbleOutline, DarkModeOutlined, FullscreenExit, LanguageOutlined, ListOutlined, NotificationsNoneOutlined, SearchOutlined } from '@mui/icons-material'
 import React from 'react'
 import './navbar.scss'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlined />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlined className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExit />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutline className="icon" />
+            <div className="counter">6</div>
+          </div>
+          <div className="item">
+            <ListOutlined className="icon" />
+          </div>
+          <div className="item">
+            <img 
+              src="https://images.pexels.com/photos/6568189/pexels-photo-6568189.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+              alt="" 
+              className="avatar" 
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import './home.scss'
 
@@ -6,7 +7,10 @@ const Home = () => {
   return (
     <div className="home">
         <Sidebar/>
-        <div className="homeContainner">container</div>
+        <div className="homeContainner">
+          <Navbar/>
+          container
+        </div>
     </div>
   )
 }
