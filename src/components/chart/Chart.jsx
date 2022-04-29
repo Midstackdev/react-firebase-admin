6f88713f0cdbed11afcfdd2eb9e475180b03a261
Midstackdev <midstackdev@gmail.com>
2022-04-29 08:26:45 +0000

    chart

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { chartData as data } from '../../dummy';
+import './chart.scss'
+
+const Chart = () => {
+  return (
+    <div className="chart">
+      <div className="title">Revenue for Past 6 Months</div>
+      <ResponsiveContainer width="100%" aspect={2/1}>
+      <AreaChart width={730} height={250} data={data}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+        <defs>
+          <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+          </linearGradient>
+          {/* <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+          </linearGradient> */}
+        </defs>
+        <XAxis dataKey="name" stroke="gray"/>
+        {/* <YAxis /> */}
+        <CartesianGrid strokeDasharray="3 3" className="chartGrid"/>
+        <Tooltip />
+        <Area type="monotone" dataKey="Total" stroke="#8884d8" fillOpacity={1} fill="url(#total)" />
+        {/* <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" /> */}
+      </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default Chart
