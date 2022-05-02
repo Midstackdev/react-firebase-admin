55e4297706cd07615ab28f81632153dce07f1dfc
Midstackdev <midstackdev@gmail.com>
2022-05-02 13:36:08 +0000

    show item

--- src/pages/show/Show.jsx
+++ src/pages/show/Show.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Chart from '../../components/chart/Chart'
+import Table from '../../components/table/Table'
 import './show.scss'
 
 const Show = () => {
   return (
-    <div>Show</div>
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar/>
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img 
+                src="https://images.unsplash.com/photo-1519419691348-3b3433c4c20e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1162&q=80" 
+                alt="" 
+                className="itemImg" 
+              />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">janedoe@gm.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+233 50712 7431</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">No. 21 Adjudu Enclave, Awoshie, Accra</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">Ghana</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3/1} title="User Spending (Last 6 Months)"/>
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <Table />
+        </div>
+      </div>
+    </div>
   )
 }
 
