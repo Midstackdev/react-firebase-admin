014e667606bca55d5a7d01b2ba159b4fe8bc4218
Midstackdev <midstackdev@gmail.com>
2022-05-04 00:42:37 +0000

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './login.scss'
 
 const Login = () => {
+  const [error, setError] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!email || !password) {
+      setError(true)
+      return
+    }
+    console.log({email, password})
+    navigate("/")
+  }
+
   return (
-    <div>Login</div>
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <input type="email" placeholder="email" onChange={e => setEmail(e.target.value)} />
+        <input type="password" placeholder="password" onChange={e => setPassword(e.target.value)} />
+        <button type="submit">Login</button>
+        {error && <span>Wrong email/password</span>}
+      </form>
+    </div>
   )
 }
 
